20897f8e1cef3594d3079ab838a12bbe615a5f9d
Edison Moreno <john.moreno@rappi.com>
2023-11-25 13:28:26 -0500

    Add Config S3

--- providers.tf
+++ providers.tf
@@ -2,12 +2,18 @@ terraform {
   required_providers {
     aws = {
       source = "hashicorp/aws"
-      version = "~> 3.27"
+      version = "~> 5.0"
     }
   }
-  backend "local" {
-    path = "/Users/john.moreno/Repos/terraform/resources/itm-iac-state.tfstate"
+  backend "s3" {
+    bucket = "move-itm-iac"
+    key = "itm-iac-state.tfstate"
+    region = "us-east-1"
+    # profile = "itm" // Only to local
   }
+  # backend "local" {
+  #   path = "/Users/john.moreno/Repos/terraform/resources/itm-iac-state.tfstate"
+  # }
 }
 
 # Configure for AWS
